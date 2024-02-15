f1c8447c34f21530cda9e4ba44db119ca226432a
Divyanshisri-co <105115278+Divyanshisri-co@users.noreply.github.com>
2024-02-15 15:58:26 +0530

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -3,12 +3,12 @@ def calculate(list1):
     if len(list1)==9:
         arr=np.array(list1).reshape((3,3))
     calculations ={}
-    calculations['mean'] = [list1.mean(axis=0).tolist(),list1.mean(axis=1).tolist(),np.mean(list1).tolist()],
-    calculations['variance'] = [list1.var(axis=0).tolist(),list1.var(axis=1).tolist(),np.var(list1).tolist()],
-    calculations['standard deviation']= [list1.std(axis=0).tolist(),list1.std(axis=1).tolist(),np.std(list1).tolist()],
-    calculations['max'] = [list1.max(axis=0).tolist(),list1.max(axis=1).tolist(),np.max(list1).tolist()],
-    calculations['min'] = [list1.min(axis=0).tolist(),list1.min(axis=1).tolist(),np.min(list1).tolist()],
-    calculations['sum'] = [list1.sum(axis=0).tolist(),list1.sum(axis=1).tolist(),np.sum(list1).tolist()]
+    calculations['mean'] = [arr.mean(axis=0).tolist(),arr.mean(axis=1).tolist(),np.mean(arr).tolist()]
+    calculations['variance'] = [arr.var(axis=0).tolist(),arr.var(axis=1).tolist(),np.var(arr).tolist()]
+    calculations['standard deviation']= [arr.std(axis=0).tolist(),arr.std(axis=1).tolist(),np.std(arr).tolist()]
+    calculations['max'] = [arr.max(axis=0).tolist(),arr.max(axis=1).tolist(),np.max(arr).tolist()]
+    calculations['min'] = [arr.min(axis=0).tolist(),arr.min(axis=1).tolist(),np.min(arr).tolist()]
+    calculations['sum'] = [arr.sum(axis=0).tolist(),arr.sum(axis=1).tolist(),np.sum(arr).tolist()]
     
     return calculations
 list1=[0,1,2,3,4,5,6,7,8]
